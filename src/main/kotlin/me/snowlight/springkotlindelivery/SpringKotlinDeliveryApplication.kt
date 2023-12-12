7035743338cce436308196ceb27412408b039e6c
snowlight-aemt <it.s.aemt@gmail.com>
2023-12-12 21:52:36 +0900

    Init

---
+++ src/main/kotlin/me/snowlight/springkotlindelivery/SpringKotlinDeliveryApplication.kt
@@ -0,0 +1,11 @@
+package me.snowlight.springkotlindelivery
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringKotlinDeliveryApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringKotlinDeliveryApplication>(*args)
+}
---
+++ src/test/kotlin/me/snowlight/springkotlindelivery/SpringKotlinDeliveryApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.snowlight.springkotlindelivery
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringKotlinDeliveryApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
